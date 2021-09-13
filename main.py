73eabbbcc995e9fd0e57e038059c8746ad072a6f
Nicole Gathany <nicole.gathany@gmail.com>
2021-09-13 20:35:41 +0000

    added instructions and examples

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ). 
+
+#For example: (Input1, Input2) --> output
+
 
 
 def solution(a, b):
@@ -8,3 +12,5 @@ def solution(a, b):
 
 
 print(solution("45", "a"))
+print(solution("1", "22")) #"1221"
+print(solution("22", "1")) # "1221"
